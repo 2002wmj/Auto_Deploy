61c883bc5c1e93ad31f366aeb24e04ae6899dc5a
wmj <wangmj@lejiayuan.cn>
2017-04-05 16:28:33 +0800

    3.0版本更新

--- py_scripts/config.py
+++ py_scripts/config.py
@@ -1,5 +1,5 @@
 #-*- coding: UTF-8 -*-
 #程序相关配置
 gray_res = True  #是否开启灰度发布(True/False)
-app_ints_range = [1,2,3] #实例ID的取值范围，必须是列表
+app_ints_range = [1,2,3,4] #实例ID的取值范围，必须是列表
 full_release = True #是否开启全量发布(True/False)
